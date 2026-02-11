ecb7def76ebbd83698908bfe87ffe599d656a304
Torkild Liebe <torkildliebe@gmail.com>
2026-02-11 13:50:25 +0100

    fix#9: load web map implementation on expo web

--- apps/mobile/src/features/map/components/map-view.ts
+++ apps/mobile/src/features/map/components/map-view.ts
@@ -1,2 +1,17 @@
-export { MapView } from './map-view.native';
-export type { MapViewHandle } from './map-view.native';
+import type { ComponentType } from 'react';
+import { Platform } from 'react-native';
+import type { MapViewHandle, MapViewProps } from './map-view-types';
+
+type MapViewModule = {
+  MapView: ComponentType<MapViewProps>;
+};
+
+const mapViewModule: MapViewModule =
+  Platform.OS === 'web'
+    ? // eslint-disable-next-line @typescript-eslint/no-require-imports
+      require('./map-view.web')
+    : // eslint-disable-next-line @typescript-eslint/no-require-imports
+      require('./map-view.native');
+
+export const MapView = mapViewModule.MapView;
+export type { MapViewHandle };
